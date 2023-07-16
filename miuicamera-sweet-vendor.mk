f159bb6d71bb2e04e786480683b90f72721a59ec
basamaryan <basam.aryan@gmail.com>
2023-07-16 10:13:33 +0200

    Bring back face detection libs
    Change-Id: Ic98062c876436e945c286c0b8ea8819213da69f3

--- miuicamera-sweet-vendor.mk
+++ miuicamera-sweet-vendor.mk
@@ -5,9 +5,14 @@
 PRODUCT_SOONG_NAMESPACES += \
     vendor/xiaomi/miuicamera-sweet
 
+PRODUCT_COPY_FILES += \
+    vendor/xiaomi/miuicamera-sweet/proprietary/vendor/etc/camera/facedetectpp_0_5_2_model:$(TARGET_COPY_OUT_VENDOR)/etc/camera/facedetectpp_0_5_2_model
+
 PRODUCT_PACKAGES += \
     libcamera_algoup_jni.xiaomi \
     libcamera_mianode_jni.xiaomi \
     libmicampostproc_client \
     vendor.xiaomi.hardware.campostproc@1.0 \
+    libFaceDetectpp-0.5.2 \
+    libfacedet \
     MiuiCamera
